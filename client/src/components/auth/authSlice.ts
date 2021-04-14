f8e09ce78ec3851f4bfaf49811b8277f630e3273
Oddgrd <oddgrd@gmail.com>
2021-04-14 18:22:10 +0200

    clearState action

--- client/src/components/auth/authSlice.ts
+++ client/src/components/auth/authSlice.ts
@@ -1,16 +1,22 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import api from '../../utils/api';
 
 interface AuthState {
   isAuthenticated: boolean;
-  loading: boolean;
   user: object | null;
+  isFetching: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  errorMessage: string;
 }
 
 const initialState: AuthState = {
   isAuthenticated: false,
-  loading: true,
-  user: null
+  user: null,
+  isFetching: false,
+  isSuccess: false,
+  isError: false,
+  errorMessage: ''
 };
 
 export const authSlice = createSlice({
@@ -19,12 +25,20 @@ export const authSlice = createSlice({
   reducers: {
     loadUser: (state, action: PayloadAction<object>) => {
       state.isAuthenticated = true;
-      state.loading = false;
+      state.isSuccess = true;
       state.user = action.payload;
+    },
+    clearState: (state) => {
+      state.isAuthenticated = false;
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.user = null;
+      return state;
     }
   }
 });
 
-export const { loadUser } = authSlice.actions;
+export const { loadUser, clearState } = authSlice.actions;
 
 export default authSlice.reducer;
--- client/src/components/layout/Landing.tsx
+++ client/src/components/layout/Landing.tsx
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
-
+import { Login } from '../auth/Login';
+import { useAppSelector } from '../../app/hooks';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
   return (
     <section className='container'>
       <div className='landing'>
         <h1 style={{ color: '#05ab75', fontSize: '2.5rem' }}>Covegg-19</h1>
         <div className='landing-links'>
-          <Link to='/edit' className='landing-link'>
-            <strong>CREATE</strong>
-          </Link>
+          {isAuthenticated ? (
+            <Link to='/edit' className='landing-link'>
+              <strong>CREATE</strong>
+            </Link>
+          ) : (
+            <Login />
+          )}
+
           <Link to='/problems' className='landing-link'>
             <strong>BROWSE</strong>
           </Link>
