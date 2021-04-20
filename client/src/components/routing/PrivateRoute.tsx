3fa85ec0f7fb24e4b06b096928051096eba8004f
Oddgrd <oddgrd@gmail.com>
2021-04-20 15:43:32 +0200

    privateroute temp fix

--- client/src/components/routing/PrivateRoute.tsx
+++ client/src/components/routing/PrivateRoute.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Route, Redirect, RouteProps } from 'react-router-dom';
+import {
+  Route,
+  Redirect,
+  RouteProps,
+  RouteComponentProps
+} from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import Spinner from '../layout/Spinner';
 
-export const PrivateRoute = ({ ...routeProps }: RouteProps) => {
+export const PrivateRoute = ({
+  component: Component,
+  ...routeProps
+}: RouteProps) => {
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
   const status = useAppSelector((state) => state.auth.status);
 
-  if (status === 'pending') {
-    return <Spinner />;
-  } else if (isAuthenticated) {
-    return <Route {...routeProps} />;
-  } else {
-    return <Redirect to='/' />;
+  if (!Component) {
+    throw Error('component is undefined');
   }
+
+  // Prevents redirect on refresh, but gets stuck if logged out in route,
+  // fix using localStorage
+  const render = (props: RouteComponentProps<any>): React.ReactNode => {
+    if (status === 'pending' || status === 'idle') {
+      return <Spinner />;
+    } else if (status === 'resolved' && isAuthenticated) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to={{ pathname: '/' }} />;
+    }
+  };
+
+  return <Route {...routeProps} render={render} />;
 };
