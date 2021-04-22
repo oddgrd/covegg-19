f2b5eed58c41309fbcc928ce29ff837f50b4c2d4
Oddgrd <oddgrd@gmail.com>
2021-04-23 00:40:02 +0200

    reworked

--- client/src/components/browser/BrowserItem.tsx
+++ client/src/components/browser/BrowserItem.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
 import grades from '../editor/grades';
-import { Problem } from './browserSlice';
+import { faEllipsisV, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BrowserItemInfo } from './BrowserItemInfo';
 
 interface Props {
   problem: {
@@ -11,25 +13,50 @@ interface Props {
     title: string;
     grade: number;
     date: string;
+    rating: number;
   };
 }
 
 export const BrowserItem = ({ problem }: Props) => {
-  const firstName = problem.setBy.split(' ').slice(0, 1);
+  const [expand, toggleExpand] = useState(false);
+
   return (
-    <>
-      <tr>
-        <td>
-          <Link to={`/problem/${problem._id}`}>{problem.title}</Link>
-        </td>
-        <td>{firstName}</td>
-        <td style={{ color: `${grades[problem.grade].color}` }}>
-          {grades[problem.grade].grade}
-        </td>
-        <td>
-          <Moment fromNow>{problem.date}</Moment>
-        </td>
-      </tr>
-    </>
+    <div className='browser-item'>
+      <div className='browser-item-main'>
+        <button
+          onClick={() => toggleExpand(!expand)}
+          className='btn'
+          style={{
+            color: '#05ab75',
+            fontSize: '1.4rem',
+            padding: '1rem',
+            maxWidth: '49.8px'
+          }}
+        >
+          <FontAwesomeIcon icon={expand ? faEllipsisH : faEllipsisV} />
+        </button>
+        <div className='browser-item-title-div'>
+          <Link className='browser-item-title' to={`/problem/${problem._id}`}>
+            {problem.title}
+          </Link>
+        </div>
+        <div className='browser-item-grade unselectable'>
+          <p
+            style={{
+              color: `${grades[problem.grade].color}`
+            }}
+          >
+            {grades[problem.grade].grade}
+          </p>
+        </div>
+      </div>
+      {expand && (
+        <BrowserItemInfo
+          setBy={problem.setBy}
+          date={problem.date}
+          rating={problem.rating}
+        />
+      )}
+    </div>
   );
 };
