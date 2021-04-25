c7fdaf2a27e6132799603ec99ad42c9c00467319
Oddgrd <oddgrd@gmail.com>
2021-04-25 18:59:45 +0200

    implement svg logo in navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NavbarMenu } from './NavbarMenu';
+import logoUrl from '../../images/Logo-klatreapp.svg';
+import SVG, { Props as SVGProps } from 'react-inlinesvg';
 
+const Logo = React.forwardRef<SVGElement, SVGProps>((props, ref) => (
+  <SVG innerRef={ref} title='MyLogo' {...props} />
+));
 export const Navbar = () => {
+  const logo = useRef<SVGElement>(null);
   return (
     <nav className='navbar'>
       <NavbarMenu />
 
-      <h1 style={{ textAlign: 'center' }}>
-        <Link to='/' className='nav-link'>
-          <strong>C-19</strong>
-        </Link>
-      </h1>
+      <Link to='/' className='nav-link'>
+        <SVG src={`${process.env.PUBLIC_URL}/menu.svg`} title='Menu' />
+        <Logo ref={logo} src={logoUrl} style={{ width: '48px' }} />
+      </Link>
+
       <Link to='/browse' style={{ textAlign: 'end' }}>
         <FontAwesomeIcon icon={faList} className='nav-link' />
       </Link>
