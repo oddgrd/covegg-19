d776105db67d36354e8eb9849f258d79b15a16f4
Oddgrd <oddgrd@gmail.com>
2021-05-06 16:06:50 +0200

    heroku

--- src/server.ts
+++ src/server.ts
@@ -151,11 +151,12 @@ app.use((_req, res, _next) => {
 });
 
 // Serve static assets in production
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (_req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+
 const httpServer = http.createServer(app);
 httpServer.listen(config.server.port, () =>
   logging.info(
