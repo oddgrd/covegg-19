6e3fddbafcb7823c4ed74378adee2a579fb62686
Oddgrd <oddgrd@gmail.com>
2021-05-06 15:55:04 +0200

    heroku

--- src/server.ts
+++ src/server.ts
@@ -151,15 +151,11 @@ app.use((_req, res, _next) => {
 });
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-  app.get('*', (_req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 const httpServer = http.createServer(app);
 httpServer.listen(config.server.port, () =>
   logging.info(
