4bdfae5810db562dc239df3d7a6bad62bbb5f195
Oddgrd <oddgrd@gmail.com>
2021-05-13 16:29:15 +0200

    fix editor loading bug, remove dropdown form

--- client/src/components/editor/Editor.tsx
+++ client/src/components/editor/Editor.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas } from './Canvas';
 import { useCanvas } from '../../hooks/useCanvas';
 import { Toolbar } from './Toolbar';
 import EditorForm from './EditorForm';
-import { faUndo, faSave } from '@fortawesome/free-solid-svg-icons';
+import { faUndo } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppSelector } from '../../app/hooks';
 import { Board } from '../board/boardSlice';
@@ -13,19 +13,9 @@ import Spinner from '../layout/Spinner';
 const Editor = () => {
   const [{ canvas, coords }, { init, handleColor, undo }] = useCanvas();
   const [currentBoard, setCurrentBoard] = useState<Board | null>(null);
-  const [editorForm, toggleEditorForm] = useState(false);
-  const scrollRef = useRef<HTMLDivElement>(null);
 
   const boards = useAppSelector((state) => state.board.boards);
   const status = useAppSelector((state) => state.board.status);
-
-  const toggleForm = () => {
-    toggleEditorForm(!editorForm);
-  };
-
-  useEffect(() => {
-    if (editorForm) scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [editorForm]);
 
   useEffect(() => {
     if (!init) return;
@@ -34,45 +24,38 @@ const Editor = () => {
 
   return (
     <section className='container'>
-      <BoardSelector boards={boards} setCurrentBoard={setCurrentBoard} />
-      <div className='editor'>
-        <Toolbar handleColor={handleColor} />
-        <div
-          className='board'
-          style={{
-            backgroundImage: `url(${
-              status === 'pending' ? (
-                <Spinner />
-              ) : status === 'resolved' ? (
-                currentBoard ? (
-                  currentBoard.imageUrl
-                ) : (
-                  boards[0].imageUrl
-                )
-              ) : (
-                'none'
-              )
-            })`
-          }}
-        >
-          <Canvas canvasRef={canvas} />
-        </div>
-      </div>
-      <div className='options'>
-        <button className={'btn-save btn-undo'} onClick={undo}>
-          <FontAwesomeIcon icon={faUndo} />
-        </button>
-        <button onClick={() => toggleForm()} className={'btn-save'}>
-          <FontAwesomeIcon icon={faSave} />
-        </button>
-      </div>
-      {editorForm && (
-        <EditorForm
-          coords={coords}
-          currentBoard={currentBoard ? currentBoard._id : boards[0]._id}
-        />
+      {status === 'pending' ? (
+        <Spinner />
+      ) : status === 'resolved' ? (
+        <>
+          <BoardSelector boards={boards} setCurrentBoard={setCurrentBoard} />
+          <div className='editor'>
+            <Toolbar handleColor={handleColor} />
+            <div
+              className='board'
+              style={{
+                backgroundImage: `url(${
+                  currentBoard ? currentBoard.imageUrl : boards[0].imageUrl
+                })`
+              }}
+            >
+              <Canvas canvasRef={canvas} />
+            </div>
+          </div>
+          <div className='options'>
+            <button className={'btn-save btn-undo'} onClick={undo}>
+              <FontAwesomeIcon icon={faUndo} />
+            </button>
+          </div>
+
+          <EditorForm
+            coords={coords}
+            currentBoard={currentBoard ? currentBoard._id : boards[0]._id}
+          />
+        </>
+      ) : (
+        'Error retrieving content, please return to homepage and try again'
       )}
-      <div ref={scrollRef}></div>
     </section>
   );
 };
